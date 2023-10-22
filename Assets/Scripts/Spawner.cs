4c1ca5ee5896db2d1b349c93388c5f3b340eb706
Bart <kmiecikjob@gmail.com>
2023-10-22 11:30:24 +0200

    Initail commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Spawner : MonoBehaviour
+{
+    public Vector2 widthAndHeight = new Vector2 (0, 0);
+    public GameObject prefabToSpawn = null;
+
+
+    public void SpawnObjects()
+    {
+        Debug.Log("Spawner");
+        if(transform.childCount > 0)
+        {
+            foreach (Transform child in transform)
+            {
+                DestroyImmediate(child.gameObject);
+            }
+        }
+        Debug.Log(widthAndHeight);
+        for (int width = 0; width < widthAndHeight.x; width++)
+        {
+            for (int height = 0;height<widthAndHeight.y; height++)
+            {
+                Instantiate(prefabToSpawn, new Vector3(width, height, 0), Quaternion.identity, transform);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Spawner))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Spawner myScript = (Spawner)target;
+        if (GUILayout.Button("Spawn"))
+        {
+            myScript.SpawnObjects();
+        }
+    }
+}
